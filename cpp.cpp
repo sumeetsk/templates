170b5089f9525ea9910486a5ba1dc8ff27d464f8
Sumeet Katariya <sumeet@rotor.cs.wisc.edu>
2013-09-27 13:57:47 -0500

    cpp

---
+++ cpp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+#include<map>
+using namespace std;
+#define M 10000
+#define tr(c,i) for(typeof(c.begin()) i=c.begin(); i!=c.end(); ++i)
+
+int main() {
+    return 0;
+}
